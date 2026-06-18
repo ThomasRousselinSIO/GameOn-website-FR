29f875fec81355c8df8db11c6b1485b410369d25
ThomasRousselinSIO <thomasbtssiosisr@gmail.com>
2026-06-18 13:52:57 +0200

    Final

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,17 +26,17 @@ closeBtn.addEventListener("click", function () {
 });
 
 // Afficher une erreur
-function showError(input, message) {
+function showErrors(input, message) {
   var formData = input.parentElement;
   formData.setAttribute("data-error", message);
   formData.setAttribute("data-error-visible", "true");
 }
 
 // Enlever les erreurs
-function delError() {
-  var error = document.querySelectorAll(".formData");
+function clearErrors() {
+  var errors = document.querySelectorAll(".formData");
 
-  error.forEach(function (error) {
+  errors.forEach(function (error) {
     error.removeAttribute("data-error");
     error.removeAttribute("data-error-visible");
   });
@@ -44,7 +44,8 @@ function delError() {
 
 // Validation du formulaire
 function validate() {
-  delError();
+  console.log("Validation en cours...");
+  clearErrors();
   //prenom, nom, email, date de naissance, nombre, ville choisie, conditions générales
   var first = document.getElementById("first");
   var last = document.getElementById("last");
@@ -57,27 +58,27 @@ function validate() {
   var validate = true;
   //Validation du prénom
   if (first.value.trim().length < 2) {
-    showError(first, "Le prénom doit contenir au moins 2 caractères.");
+    showErrors(first, "Le prénom doit contenir au moins 2 caractères.");
     validate = false;
   }
   //Validation du nom
   if (last.value.trim().length < 2) {
-    showError(last, "Le nom doit contenir au moins 2 caractères.");
+    showErrors(last, "Le nom doit contenir au moins 2 caractères.");
     validate = false;
   }
   //Validation de l'email
   if (email.value.trim() === "" || !email.value.trim().includes("@")) {
-    showError(email, "Veuillez entrer un email valide.");
+    showErrors(email, "Veuillez entrer un email valide.");
     validate = false;
   }
   //Validation de la date de naissance
   if (birthdate.value === "") {
-    showError(birthdate, "Veuillez entrer une date de naissance.");
+    showErrors(birthdate, "Veuillez entrer une date de naissance.");
     validate = false;
   }
   //Validation du nombre de tournois
   if (quantity.value === "" || quantity.value < 0) {
-    showError(quantity, "Veuillez entrer un nombre valide.");
+    showErrors(quantity, "Veuillez entrer un nombre valide.");
     validate = false;
   }
 
@@ -90,12 +91,12 @@ function validate() {
   });
   
   if (chosenCity === false) {
-    showError(locations[0], "Veuillez choisir une ville.");
+    showErrors(locations[0], "Veuillez choisir une ville.");
     validate = false;
   }
 
   if (checkbox1.checked === false) {
-    showError(checkbox1, "Vous devez accepter les conditions.");
+    showErrors(checkbox1, "Vous devez accepter les conditions.");
     validate = false;
   }
 
